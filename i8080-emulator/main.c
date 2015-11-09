86debbfca41ae64d2367dfa995457afc2ca8bfe2
Shaun Karran <shaun.karran@gmail.com>
2015-11-09 16:55:55 +1000

    Initial Commit

---
+++ i8080-emulator/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  i8080-emulator
+//
+//  Created by Shaun Karran on 9/11/2015.
+//  Copyright © 2015 Shaun Karran. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
