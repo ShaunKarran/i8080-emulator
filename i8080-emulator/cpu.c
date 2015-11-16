feafc9ba0b844e2ed0a336072a7dd352f22663de
Shaun Karran <shaun.karran@gmail.com>
2015-11-16 23:12:30 +1000

    Changed structure of emulator to use array of function pointers. Began defining instructions.

--- i8080-emulator/cpu.c
+++ i8080-emulator/cpu.c
@@ -7,3 +7,262 @@
 //
 
 #include "cpu.h"
+
+/* No operations are performed. */
+void NOP() { ; }
+
+/* Processor is stopped. */
+void HLT()
+{
+    // TODO
+}
+
+/* Carry flag is set to 1. */
+void STC()
+{
+    cpu.flags &= CARRY_FLAG;
+}
+
+/* Carry flag is flipped. */
+void CMC()
+{
+    cpu.flags ^= CARRY_FLAG;
+}
+
+/* Contents of register r is incremented by 1. */
+void INR(char r)
+{
+    r += 1;
+    // TODO: What happens to flags?
+}
+
+/* Contents of register r is decremented by 1. */
+void DCR(char r)
+{
+    r -= 1;
+    // TODO: What happens to flags?
+}
+
+/* Contents of the accumulator is complemented. */
+void CMA()
+{
+    cpu.a = ~cpu.a;
+}
+
+/* The eight-bit number in the accumulator is adjusted to form two four-bit
+ *  Binary-Coded-Decimal digits by the following process:
+ * If the value of the least significant 4 bits of the accumulator is greater
+ *  than 9 or if the AC flag is set, 6 is added to the accumulator.
+ * If the value of the most significant 4 bits of the accumulator is now greater
+ *  than 9, or if the CY flag is set, 6 is added to the most significant 4 bits
+ *  of the accumulator.
+ */
+void DAA()
+{
+    if (cpu.flags & AUX_CARRY_FLAG || (cpu.a & 0x0F) > 0x09)
+    {
+        cpu.a += 6;
+    }
+    if (cpu.flags & CARRY_FLAG || (cpu.a >> 4) > 0x09)
+    {
+        cpu.a += (0x06 << 4);
+    }
+}
+
+/* Contents of r2 is moved into r1. */
+void MOV(char r1, char r2)
+{
+    r1 = r2;
+}
+
+/*  */
+void STAX(char r)
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
+
+/*  */
+void NOP()
+{
+    // TODO
+}
--- i8080-emulator/cpu.h
+++ i8080-emulator/cpu.h
@@ -9,6 +9,129 @@
 #ifndef cpu_h
 #define cpu_h
 
-#include <stdio.h>
+#define ZERO_FLAG      (1 << 0)
+#define SIGN_FLAG      (1 << 1)
+#define PARITY_FLAG    (1 << 2)
+#define CARRY_FLAG     (1 << 3)
+#define AUX_CARRY_FLAG (1 << 4)
+
+struct i8080 {
+    char b, c;
+    char d, e;
+    char h, l;
+    char a;
+    char flags;
+    char w, z;
+};
+
+struct i8080 cpu;
+
+/* NOP instruction. */
+void NOP();
+
+/* HLT instruction. */
+void HLT();
+
+/* Carry bit instructions. */
+void STC();
+void CMC();
+
+/* Single register instructions. */
+void INR(char r);
+void DCR(char r);
+void CMA();
+void DAA();
+
+/* Data transfer instructions. */
+void MOV(char r1, char r2);
+
+void STAX_B(); //   0x02
+void STAX_D(); //   0x12
+
+void LDAX_B(); //   0x0A
+void LDAX_D(); //   0x1A
+
+void ADD(); //  0x00
+void ADC(); //  0x00
+void SUB(); //  0x00
+void SBB(); //  0x00
+void ANA(); //  0x00
+void XRA(); //  0x00
+void ORA(); //  0x00
+void CMP(); //  0x00
+/* Rotate accumulator instructions. */
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+/* Register pair instructions. */
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+/* Immediate instructions. */
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+/* Direct addressing instructions. */
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+/* Jump instructions. */
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+/* Call instructions. */
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+/* Return instructions. */
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+void NOP(); //  0x00
+
+/* RST instruction. */
+void RST(); //  0x00
+
+/* Interrupt flip-flop instructions. */
+void NOP(); //  0x00
+void NOP(); //  0x00
+/* Input/Output instructions. */
+void NOP(); //  0x00
+void NOP(); //  0x00
+
+void NOP(); //  0x00
+void NOP(); //  0x00
 
 #endif /* cpu_h */
